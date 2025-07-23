e38dd7095058b3a917caab462619471ccf9d1a0e
skaiTF <68827456+skaiTF@users.noreply.github.com>
2025-07-23 18:38:18 +0100

    Fix script preventing the replay buffer from being manually stopped

--- anti-overlap.py
+++ anti-overlap.py
@@ -3,6 +3,9 @@ import time
 
 hotkey_id = obs.OBS_INVALID_HOTKEY_ID
 
+# Flag to indicate if the script is handling an automatic stop/restart
+auto_restarting = False
+
 def on_smart_save_pressed(pressed):
     if not pressed:
         return
@@ -13,7 +16,9 @@ def on_smart_save_pressed(pressed):
 
     print("Save replay")
 
+    global auto_restarting
     obs.obs_frontend_replay_buffer_save()
+    auto_restarting = True
     # Wait 500ms before stopping the buffer to allow save to complete
     obs.timer_add(stop_replay_buffer_after_save, 500)
 
@@ -30,13 +35,14 @@ restart_attempts = 0
 MAX_RESTART_ATTEMPTS = 10
 
 def restart_replay_buffer():
-    global restart_attempts
+    global restart_attempts, auto_restarting
     print("Restart buffer")
     obs.obs_frontend_replay_buffer_start()
     if obs.obs_frontend_replay_buffer_active():
         print("Replay buffer restarted successfully")
         obs.timer_remove(wait_for_buffer_stop_and_restart)
         restart_attempts = 0
+        auto_restarting = False
     else:
         restart_attempts += 1
         if restart_attempts < MAX_RESTART_ATTEMPTS:
@@ -45,12 +51,15 @@ def restart_replay_buffer():
             print("Failed to restart replay buffer after multiple attempts.")
             obs.timer_remove(wait_for_buffer_stop_and_restart)
             restart_attempts = 0
+            auto_restarting = False
 
 
-# Wait for the replay buffer to stop before restarting
 def wait_for_buffer_stop_and_restart():
-    if not obs.obs_frontend_replay_buffer_active():
+    global auto_restarting
+    if auto_restarting and not obs.obs_frontend_replay_buffer_active():
         restart_replay_buffer()
+    elif not auto_restarting:
+        obs.timer_remove(wait_for_buffer_stop_and_restart)
 
 
 def script_load(settings):
